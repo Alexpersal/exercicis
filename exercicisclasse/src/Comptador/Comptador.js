ab0e6d0f2d73d968edc9e42a365859f39c2eed55
Alexpersal <97981869+Alexpersal@users.noreply.github.com>
2022-02-17 12:37:09 +0100

    exercicis

--- exercicisclasse/src/Comptador/Comptador.js
+++ exercicisclasse/src/Comptador/Comptador.js
@@ -1,28 +1,36 @@
-import { useState } from "react";
 import "./Comptador.css";
-
+import { useState } from "react";
 const operador = ["+1", "-1", "+5"];
 
-function Comptador() {
+export default function App() {
   const [incrementa, setIncrementa] = useState(0);
   //const [calculo, setcalculo] = useState("");
 
-  function Display() {
-    setIncrementa(incrementa + 1);
-    //if (calculo === "+1" && incrementa < 10) setIncrementa(incrementa + 1);
-    console.log(incrementa);
-    if (incrementa < 10) setIncrementa(incrementa + 1);
-    //if (incrementa > 0) setIncrementa(incrementa - 1);
+  function Suma() {
+    incrementa < 10
+      ? setIncrementa(incrementa + 1)
+      : alert("no puede sumarse ya que el n úmero seria mayor a 10");
+  }
+  function Resta() {
+    incrementa > 0
+      ? setIncrementa(incrementa - 1)
+      : alert("no puede sumarse ya que el n úmero seria menor a 0");
+  }
+  function Suma5() {
+    incrementa < 6
+      ? setIncrementa(incrementa + 5)
+      : alert("no puede sumarse ya que el n úmero seria mayor a 10");
   }
   return (
     <div className="Comptador">
-      <div className="Display"> {incrementa} </div>
-      <br />
-      {operador.map((n, i) => (
-        <button onClick={Display()}>{n}</button>
-      ))}
+      <div className="Calculadora">
+        <div className="Display"> {incrementa} </div>
+        <div className="Botones">
+          <button onClick={Suma}>+1</button>
+          <button onClick={Resta}>-1</button>
+          <button onClick={Suma5}>+5</button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default Comptador;
