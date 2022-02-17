612f5e830a463d9b2e42cd08a4fe5a50a6b80f43
Alexperezsal <al.pes79@gmail.com>
2022-02-17 17:39:52 +0100

    exercicis

--- exercicisclasse/src/Comptador/Comptador.js
+++ exercicisclasse/src/Comptador/Comptador.js
@@ -5,24 +5,32 @@ const operador = ["+1", "-1", "+5"];
 
 function Comptador() {
   const [incrementa, setIncrementa] = useState(0);
-  //const [calculo, setcalculo] = useState("");
 
-  function Display() {
-    setIncrementa(incrementa + 1);
-    //if (calculo === "+1" && incrementa < 10) setIncrementa(incrementa + 1);
-    console.log(incrementa);
-    if (incrementa < 10) setIncrementa(incrementa + 1);
-    //if (incrementa > 0) setIncrementa(incrementa - 1);
+  function Calcular(n) {
+    if (incrementa < 10) {
+      if (n === "+1") setIncrementa((num) => num + 1);
+      if (incrementa < 6) {
+        if (n === "+5") setIncrementa((num) => num + 5);
+      }
+    }
+    if (incrementa > 0) {
+      if (n === "-1") setIncrementa((num) => num - 1);
+    }
   }
+
   return (
     <div className="Comptador">
-      <div className="Display"> {incrementa} </div>
-      <br />
-      {operador.map((n, i) => (
-        <button onClick={Display()}>{n}</button>
-      ))}
+      <div className="Calculadora">
+        <div className="Display"> {incrementa} </div>
+        <div className="Botones">
+          {operador.map((n) => (
+            <button className={n} onClick={() => Calcular(n)}>
+              {n}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
-
 export default Comptador;
