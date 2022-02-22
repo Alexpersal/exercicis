1539ace10ffc65c3e84a21dbbc4e6785619d6068
Alexpersal <97981869+Alexpersal@users.noreply.github.com>
2022-02-22 11:13:13 +0100

    exercicis

--- exercicisclasse/src/App.js
+++ exercicisclasse/src/App.js
@@ -1,15 +1,15 @@
 //import Galeria from "./Galeria/Galeria";
 //import Comptador from "./Comptador/Comptador";
-//import Carrusel from "./Carrusel/Carrusel";
-import Acordio from "./Acordio/Acordio";
-import ToggleButton from "./ToggleButton/ToggleButton";
+import Carrusel from "./Carrusel/Carrusel";
+//import Acordio from "./Acordio/Acordio";
+//import ToggleButton from "./ToggleButton/ToggleButton";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ToggleButton></ToggleButton>
+      <Carrusel></Carrusel>
     </div>
   );
 }
--- exercicisclasse/src/Carrusel/Carrusel.js
+++ exercicisclasse/src/Carrusel/Carrusel.js
@@ -7,27 +7,21 @@ function Carrusel() {
     "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
     "https://cdn.pixabay.com/photo/2016/09/07/11/37/sunset-1651426_960_720.jpg",
   ];
-  const [imgNext, setImgNext] = useState(
+  /*const [imgNext, setImgNext] = useState(
     "https://cdn.pixabay.com/photo/2017/03/29/15/18/tianjin-2185510_960_720.jpg"
-  );
+  );*/
   const [posicio, setPosicio] = useState(0);
 
   function siguiente() {
-    console.log(posicio);
-
-    console.log(posicio);
-    /*if (posicio < 3) {
-      setImgNext(imagen[posicio]);
-    }*/
+    if (posicio < imagen.length - 1) {
+      console.log(posicio);
+      setPosicio((posicio) => posicio + 1);
+      console.log(posicio);
+    }
   }
 
   function previa() {
-    console.log(posicio);
-    setPosicio((posicio) => posicio - 1);
-    console.log(posicio);
-    if (posicio > 0) {
-      setImgNext(imagen[posicio]);
-    }
+    if (posicio > 0) setPosicio(posicio - +1);
   }
   return (
     <div className="Container">
@@ -35,7 +29,7 @@ function Carrusel() {
         boton izquierdo
       </button>
       <div className="Imagenes">
-        <img className="Img" alt="imag1" src={imgNext}></img>
+        <img className="Img" alt="imag1" src={imagen[posicio]}></img>
       </div>
       <button className="Der" onClick={() => siguiente()}>
         Boton derecho
