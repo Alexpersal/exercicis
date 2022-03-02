b38daebb9eebdcaee90c74dc39be3d3b89eefda1
Alexperezsal <al.pes79@gmail.com>
2022-03-02 16:25:24 +0100

    exercicis

--- exercicisclasse/src/pages/TresEnRalla/TresEnRalla.js
+++ exercicisclasse/src/pages/TresEnRalla/TresEnRalla.js
@@ -4,52 +4,35 @@ import "./TresEnRalla.css";
 function TresEnRalla() {
   let inicio = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
   const [simbolo, setSimbolo] = useState("-");
-  //const [clicado, setClicado] = useState(null);
+  const [clicado, setClicado] = useState(null);
 
-  function cambiaEstado(n) {
-    console.log(n);
-    console.log(n);
-    //if (simbolo === inicio[n]) {
-    setSimbolo("X");
-    inicio[n] = simbolo;
-    console.log(inicio[n]);
-    // }
-    /*  if (n === "2") {
-      simbolo === inicio[+n] ? setSimbolo("X") : setSimbolo("-");
-    }*/
+  function cambiaEstado(i) {
+    setClicado(i);
+    if (inicio[i] === "-") {
+      if (i % 2 === 0) {
+        setSimbolo("X");
+        setClicado(i);
+      } else {
+        setSimbolo("Y");
+        setClicado("Y");
+      }
+      inicio[i] = simbolo;
+    }
+    console.log(simbolo);
+    console.log(clicado);
   }
 
   return (
-    <div>
-      <div className="Tablero">
-        <button className="0" key="0" onClick={() => cambiaEstado("0")}>
-          {inicio}
-        </button>
-        <button className="1" key="1" onClick={() => cambiaEstado("1")}>
+    <div className="Tablero">
+      {inicio.map((n, i) => (
+        <button
+          className={n === clicado ? simbolo : "Z"}
+          key={i}
+          onClick={() => cambiaEstado(i)}
+        >
           {simbolo}
         </button>
-        <button className="2" key="2" onClick={() => cambiaEstado()}>
-          {simbolo}
-        </button>
-        <button className="3" key="3" onClick={() => cambiaEstado()}>
-          {simbolo}
-        </button>
-        <button className="4" key="4" onClick={() => cambiaEstado()}>
-          {simbolo}
-        </button>
-        <button className="5" key="5" onClick={() => cambiaEstado()}>
-          {simbolo}
-        </button>
-        <button className="6" key="6" onClick={() => cambiaEstado()}>
-          {simbolo}
-        </button>
-        <button className="7" key="7" onClick={() => cambiaEstado()}>
-          {simbolo}
-        </button>
-        <button className="8" key="8" onClick={() => cambiaEstado()}>
-          {simbolo}
-        </button>
-      </div>
+      ))}
     </div>
   );
 }
