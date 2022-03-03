227c4747246cffaa448bf5bf66bfa3f0f669a006
Alexperezsal <al.pes79@gmail.com>
2022-03-03 20:41:41 +0100

    exercicis

--- exercicisclasse/src/pages/Carrusel/Carrusel.js
+++ exercicisclasse/src/pages/Carrusel/Carrusel.js
@@ -4,25 +4,29 @@ import { useState } from "react";
 function Carrusel() {
   const imagen = [
     "https://cdn.pixabay.com/photo/2017/03/29/15/18/tianjin-2185510_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
     "https://cdn.pixabay.com/photo/2016/09/07/11/37/sunset-1651426_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
   ];
-  /*const [imgNext, setImgNext] = useState(
-    "https://cdn.pixabay.com/photo/2017/03/29/15/18/tianjin-2185510_960_720.jpg"
-  );*/
+
   const [posicio, setPosicio] = useState(0);
+  const [resalta, setResalta] = useState("sinResaltar");
 
   function siguiente() {
     if (posicio < imagen.length - 1) {
-      console.log(posicio);
       setPosicio((posicio) => posicio + 1);
-      console.log(posicio);
     }
   }
 
   function previa() {
     if (posicio > 0) setPosicio(posicio - +1);
   }
+
+  function cambiaImagen(i) {
+    resalta === "sinResaltar"
+      ? setResalta("resaltar")
+      : setResalta("sinResaltar");
+  }
+
   return (
     <body>
       <div className="Container">
@@ -38,7 +42,7 @@ function Carrusel() {
       </div>
       <div className="slide">
         {imagen.map((n, i) => (
-          <div className={n} key={i}>
+          <div className={n === posicio ? "resalta" : "sinResaltar"} key={i}>
             o
           </div>
         ))}
