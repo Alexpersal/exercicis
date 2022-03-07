f1a79db6d6ba62f369d924cb0119f3e4caa24e1d
Alexperezsal <al.pes79@gmail.com>
2022-03-07 20:58:23 +0100

    exercicis

---
+++ exercicisclasse/src/pages/SumadorNumeros/SumadorNumeros.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function SumadorNumeros() {
+  const [suma, setSuma] = useState();
+
+  function sumar() {}
+  return (
+    <div>
+      <label for="valor">Valor: </label>
+      <input type="Text" id="valor" name="valor"></input>
+      <label for="total">Total: </label>
+      <input type="Number" id="total" name="total" placeholder="0"></input>
+      <h1>
+        <button onClick={() => sumar()}>Suma</button> suma
+      </h1>
+    </div>
+  );
+}
