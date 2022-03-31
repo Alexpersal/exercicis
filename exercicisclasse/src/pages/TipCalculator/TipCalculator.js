25567be710b7c57d24571c2b4f2b312811a440ac
Alexperezsal <al.pes79@gmail.com>
2022-03-31 18:39:40 +0200

    Tip Calculator

--- exercicisclasse/src/pages/TipCalculator/TipCalculator.js
+++ exercicisclasse/src/pages/TipCalculator/TipCalculator.js
@@ -1,27 +1,55 @@
 import { useState } from "react";
 
 export default function TipCalculator() {
-  const [importeIni, setImporteIni] = useState();
+  const [importeInicial, setImporteInicial] = useState();
+  const [tip, setTip] = useState(0);
   const [suma, setSuma] = useState(0);
 
-  const totalCuenta = (importeIni) => {
-    setSuma(parseInt(importeIni) + parseInt(suma));
+  const totalCuenta = (importeIni, tip) => {
+    const propina = (importeIni * tip) / 100;
+    setSuma(parseFloat(importeIni) + propina);
+    console.log(propina);
+    console.log(suma);
+  };
+
+  const redondeo = () => {
+    if (suma % 5 === 0) console.log(suma % 5);
+    else return console.log("capullo");
   };
 
   return (
     <>
       <h2>Calcular cuenta Restaurante</h2>
-      <label className="Factura">Cuenta Restaurante:</label>
+      <label className="factura">Cuenta Restaurante:</label>
       <input
-        value={importeIni}
+        value={importeInicial}
         type={Number}
-        onChange={(event) => setImporteIni(event.target.value)}
+        onChange={(event) => setImporteInicial(event.target.value)}
       />
+      €
       <br />
-      <label className="sumaTotal">Propina: </label>
-      <input value={suma + "%"} type={Number} />
+      <label className="propina">Cantidad Propina: </label>
+      <input
+        value={tip}
+        type={Number}
+        onChange={(event) => setTip(event.target.value)}
+      />
+      %
       <br />
-      <button onClick={() => totalCuenta(importeIni)}>Sumar</button>
+      <label className="TotalCuenta">Total cuenta con propina: </label>
+      <input value={suma} type={Number} />
+      €
+      <br />
+      <button onClick={() => totalCuenta(importeInicial, tip)}>Sumar</button>
+      <div>
+        <input
+          type="checkbox"
+          name="redondeo"
+          id="opt-in"
+          onClick={() => redondeo()}
+        />
+        <label for="opt-in">Redondear a 5€</label>
+      </div>
     </>
   );
 }
