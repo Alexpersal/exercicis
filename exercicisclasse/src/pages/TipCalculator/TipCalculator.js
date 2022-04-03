b1a521e759247ca27aed90438440e510a83159d7
Alexpersal <97981869+Alexpersal@users.noreply.github.com>
2022-04-03 20:18:54 +0200

    TipCalculator

--- exercicisclasse/src/pages/TipCalculator/TipCalculator.js
+++ exercicisclasse/src/pages/TipCalculator/TipCalculator.js
@@ -1,83 +1,90 @@
 import { useState } from "react";
+import "./TipCalculator.css";
 
 export default function TipCalculator() {
   const [importeInicial, setImporteInicial] = useState();
   const [tip, setTip] = useState(0);
   const [suma, setSuma] = useState(0);
-  const [redondeo, setRedondeo] = useState(0);
+  const [redondeo, setRedondeo] = useState(suma);
   const [incrementa, setIncrementa] = useState(1);
 
   const totalCuenta = (importeIni, tip) => {
     const propina = (importeIni * tip) / 100;
     setSuma(parseFloat(importeIni) + propina);
-    console.log(propina);
-    console.log(suma);
   };
 
   const redondear = () => {
     if (suma % 5 === 0) setRedondeo(suma + 5);
     else {
-      //no funciona todavia el redondeo si no es 5 o 10
       setRedondeo(Math.ceil(suma));
+      let propina = Math.ceil(suma);
       console.log(redondeo + "redondeo");
-      while (redondeo % 5 !== 0) setRedondeo((num) => num + 1);
+      console.log(propina + "propina");
+      setSuma(propina);
+
+      while (propina % 5 !== 0) {
+        propina = propina + 1;
+        setRedondeo(propina);
+        setSuma(propina);
+        console.log(propina + "propina2");
+      }
     }
   };
 
   function comensales(n) {
-    if (incrementa < 10) {
-      if (n === "+1") setIncrementa((num) => num + 1);
-    }
-    if (incrementa > 1) {
-      if (n === "-1") setIncrementa((num) => num - 1);
-    }
+    setIncrementa(Math.max(1, Math.min(10, incrementa + +n)));
   }
 
   return (
-    <>
+    <div className="tipCalculator">
       <h2>Calcular cuenta Restaurante</h2>
-      <label className="factura">Cuenta Restaurante:</label>
-      <input
-        value={importeInicial}
-        type={Number}
-        onChange={(event) => setImporteInicial(event.target.value)}
-      />
-      €
-      <br />
-      <label className="propina">Cantidad Propina: </label>
-      <input
-        value={tip}
-        type={Number}
-        onChange={(event) => setTip(event.target.value)}
-      />
-      %
-      <br />
-      <label className="TotalCuenta">Total cuenta con propina: </label>
-      <input value={suma} type={Number} />
-      €
-      <br />
-      <button onClick={() => totalCuenta(importeInicial, tip)}>Sumar</button>
-      <div>
+      <div className="inicial">
+        <label className="factura">Cuenta Restaurante:</label>
+        <input
+          value={importeInicial}
+          type={Number}
+          onChange={(event) => setImporteInicial(event.target.value)}
+        />
+        €
+        <br />
+        <label className="propina">Cantidad Propina: </label>
+        <input
+          value={tip}
+          type={Number}
+          onChange={(event) => setTip(event.target.value)}
+        />
+        %
+        <br />
+        <label className="TotalCuenta">Total cuenta con propina: </label>
+        <input value={suma} type={Number} />€
+        <br />
+        <button onClick={() => totalCuenta(importeInicial, tip)}>Sumar</button>
+      </div>
+      <div className="redondear">
         <input
           type="checkbox"
           name="redondeo"
           id="opt-in"
           onClick={() => redondear()}
         />
-        <label for="opt-in">Redondear a 5€</label>
+        <label for="opt-in">Redondear hasta 5€</label>
         <br />
         <label for="opt-in">Total con redondeo {redondeo}€</label>
       </div>
-      <div>
+      <div className="numeroPax">
         <div className="Display"> {incrementa} </div>
-        <button className="sumar" onClick={() => comensales("+1")}>
-          +1
-        </button>
-        <button className="restar" onClick={() => comensales("-1")}>
-          -1
-        </button>
+        <div className="botonesPax">
+          <button className="sumar" onClick={() => comensales("+1")}>
+            +1
+          </button>
+          <button className="restar" onClick={() => comensales("-1")}>
+            -1
+          </button>
+        </div>
       </div>
-      <div>Cuenta final por persona: {suma / incrementa}</div>
-    </>
+      <div className="final">
+        Cuenta final por persona: {suma / incrementa}€
+      </div>
+    </div>
   );
 }
